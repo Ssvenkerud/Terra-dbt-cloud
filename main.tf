ff192b10b5f065551b7c4be9e6ec81221386887e
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-28 16:31:16 +0100

    WIP

--- main.tf
+++ main.tf
@@ -28,18 +28,7 @@ source = "./enviroments"
 
 
 module "git_repositories" {
-source = "./git-repositories"
+source = "./git_repositories"
 }
 
-resource "dbtcloud_global_connection" "redshift_provisioned" {
-  for_each = { for conn in var.dbt_cloud_redshift_connections : conn.name => conn } 
-  name = each.key
-  private_link_endpoint_id = each.value.private_link_endpoint_id
-  redshift = {
-    hostname = each.value.hostname
-    port     = 5439
-    // optional fields
-    dbname = each.value.dbname
-    // it is possible to set settings to connect via SSH Tunnel as well
-  }
-}
+
