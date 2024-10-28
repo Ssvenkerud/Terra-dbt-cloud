e21ba080ece3b313478fafcb3ca6c2ff0111250a
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-28 15:33:11 +0100

    WIP

--- variabels.tf
+++ variabels.tf
@@ -3,4 +3,20 @@ variable "dbt_cloud_projects" {
   default = ["demo"]
 }
 
-
+variable "dbt_cloud_redshift_connections" {
+    type = list(object({
+        name = string
+        private_link_endpoint_id = string
+        hostname = string
+        dbname = string
+  }))
+  default = [
+  {
+    name = "demo_connection"
+    private_link_endpoint_id = "dummy_link"
+    hostname = "dummy host"
+    dbname = "dev"
+  }
+  ]
+}
+ 
