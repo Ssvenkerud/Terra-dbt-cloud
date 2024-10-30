2b7cbaf07e2bd5c6e2df55962521bd474ca551d5
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-30 13:01:56 +0100

    WIP

--- connection_redshift_provisioned.tf
+++ connection_redshift_provisioned.tf
@@ -1,5 +1,4 @@
 resource "dbtcloud_global_connection" "redshift_provisioned" {
-  provider = dbtcloud
   for_each = { for conn in var.dbt_cloud_redshift_connections : conn.name => conn } 
   name = each.key
   private_link_endpoint_id = each.value.private_link_endpoint_id
