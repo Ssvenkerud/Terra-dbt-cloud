2252a0c1d922920a864f582cfebd75a4543c11d0
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-30 15:40:13 +0100

    WIP

--- enviroments.tf
+++ enviroments.tf
@@ -20,4 +20,14 @@ resource "dbtcloud_environment" "redshift_dev_environment" {
   type            = "development"
   connection_id   = dbtcloud_global_connection.redshift_provisioned[each.value.redshift_connection].id
 }
-
+resource "dbtcloud_environment" "redshift_prod_environment" {
+  for_each = { for conn in var.dbt_cloud_redshift_prod_environment :
+  conn.project =>
+  conn } 
+  dbt_version     = var.dbt_cloud_version 
+  name            = "Prod"
+  project_id      = dbtcloud_project.dbt_project[each.key].id
+  type            = "deployment"
+  deployment_type = "production"
+  connection_id   = dbtcloud_global_connection.redshift_provisioned[each.value.redshift_connection].id
+}
