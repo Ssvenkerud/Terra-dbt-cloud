90a5db094c4d49abf357fcb2862672c2d5ca6d9c
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-30 13:04:56 +0100

    WIP

--- enviroments.tf
+++ enviroments.tf
@@ -1,10 +1,11 @@
 // we can also set a deployment environment as being the production one
 resource "dbtcloud_environment" "redshift_prod_environment" {
-  for_each = { for conn in var.dbt_cloud_redshift_prod_environment : conn.name =>
+  for_each = { for conn in var.dbt_cloud_redshift_prod_environment :
+  conn.project =>
   conn } 
   dbt_version     = var.dbt_cloud_version 
   name            = "Prod"
-  project_id      = each.key
+  project_id      = dbtcloud_project.dbt_project[each.key].id
   type            = "deployment"
   deployment_type = "production"
   connection_id   = dbtcloud_global_connection.redshift_provisioned[each.value.redshift_connection].id
