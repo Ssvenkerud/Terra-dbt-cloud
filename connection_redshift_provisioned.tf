1294e14d90e5944bd9b0d6730e1042a8959937d8
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-31 14:45:57 +0100

    WIP

--- connection_redshift_provisioned.tf
+++ connection_redshift_provisioned.tf
@@ -1,5 +1,5 @@
 
-data "dbtcloud_privatelink_endpoint" "private_link_endpoint_id" {
+data "dbtcloud_privatelink_endpoint" "private_link_id" {
   for_each = { for conn in var.dbt_cloud_redshift_connections : conn.name => conn } 
 
   name = each.value.private_link_endpoint_id
@@ -8,7 +8,7 @@ data "dbtcloud_privatelink_endpoint" "private_link_endpoint_id" {
 resource "dbtcloud_global_connection" "redshift_provisioned" {
   for_each = { for conn in var.dbt_cloud_redshift_connections : conn.name => conn } 
   name = each.key
-  private_link_endpoint_id = each.value.private_link_endpoint_id
+  private_link_endpoint_id = data.dbtcloud_privatelink_endpoint.private_link_id[each.value.private_link_endpoint_id].id 
   redshift = {
     hostname = each.value.hostname
     port     = 5439
