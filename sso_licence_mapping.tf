7750e6dd37a7b8d572110464ec4690083b4cc51a
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-10-31 13:22:45 +0100

    WIP

--- sso_licence_mapping.tf
+++ sso_licence_mapping.tf
@@ -1,5 +1,5 @@
 locals {
-  developer_licence = formatlist("%sdeveloper", var.dbt_cloud_projects)
+  developer_licence = formatlist("%s developer", var.dbt_cloud_projects)
 
 }
 
@@ -7,4 +7,16 @@ locals {
 resource "dbtcloud_partial_license_map" "dev_license_map" {
   license_type               = "developer"
   sso_license_mapping_groups = local.developer_licence
+}
+
+resource "dbtcloud_partial_license_map" "it_licence" {
+  count = var.dbt_cloud_admin_enabled ? 1 : 0
+  license_type = "it"
+  sso_license_mapping_groups = ["Admin"]
+}
+
+resource "dbtcloud_partial_license_map" "read_only_licence" {
+  count = var.dbt_cloud_admin_enabled ? 1 : 0
+  license_type = "readonly"
+  sso_license_mapping_groups = ["Read only"]
 }
