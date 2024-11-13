30663c4c9f9341346aa6d8ea12e0c915376ac5c6
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-11-13 13:59:33 +0100

    WIP

--- sso_licence_mapping.tf
+++ sso_licence_mapping.tf
@@ -6,17 +6,16 @@ locals {
 # Developer license group mapping
 resource "dbtcloud_partial_license_map" "dev_license_map" {
   license_type               = "developer"
-  sso_license_mapping_groups = local.developer_licence
-}
+  sso_license_mapping_groups = concat(local.developer_licence,["App_Access_dbtCloud_Developer"])
 
 resource "dbtcloud_partial_license_map" "it_licence" {
   count = var.dbt_cloud_admin_enabled ? 1 : 0
   license_type = "it"
-  sso_license_mapping_groups = ["Admin"]
+  sso_license_mapping_groups = ["App_Access_dbtCloud_Admin"]
 }
 
 resource "dbtcloud_partial_license_map" "read_only_licence" {
   count = var.dbt_cloud_admin_enabled ? 1 : 0
   license_type = "read_only"
-  sso_license_mapping_groups = ["Read only"]
+  sso_license_mapping_groups = ["App_Access_dbtCloud_ReadOnly"]
 }
