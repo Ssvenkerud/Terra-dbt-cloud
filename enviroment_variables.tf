6df93faf129cc0afcf04178069764f6dc96eeccb
Simen Svenkerud <simen.svenkerud@norwegian.com>
2024-11-25 11:09:02 +0100

    WIP

--- enviroment_variables.tf
+++ enviroment_variables.tf
@@ -3,15 +3,17 @@ resource "dbtcloud_environment_variable" "dbt_db_env_var" {
   name       = "DBT_DB_VAR"
   project_id = dbtcloud_project.dbt_project[each.key].id
   environment_values = {
+    "Project": null
     "Dev" : "dev"
-    "Prod" : "prod"
+    "Prod" : ""
   }
 }
 resource "dbtcloud_environment_variable" "dbt_schema_env_var" {
   for_each = toset(var.dbt_cloud_projects)
-  name       = "DBT_SCHMEA_VAR"
+  name       = "DBT_SCHEMA_ENV"
   project_id = dbtcloud_project.dbt_project[each.key].id
   environment_values = {
+    "project":null
     "Dev" : "dev"
     "Prod" : "prod"
   }
