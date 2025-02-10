7f0da74fc247a9379859bf31770a944ed4f39953
Simen Svenkerud <simen@ullrscrafts.com>
2025-02-10 09:33:50 +0100

    first attempt at adding snowflake connection

---
+++ connection_snowflake.tf
@@ -0,0 +1,16 @@
+
+resource "dbtcloud_global_connection" "snowflake" {
+  for_each = { for conn in var.dbt_cloud_snowflake_connections : conn.name => conn }
+  name     = each.key
+  snowflake = {
+    account   = each.value.account
+    database  = each.value.dbname
+    warehouse = each.value.warehouse
+    role      = each.vlaue.role
+
+    client_session_keep_alive = each.value.client_session_keep_alive
+    allow_sso                 = true
+    oauth_client_id           = var.snowflake_oauth_client_id
+    oauth_client_secret       = var.snowflake_oauth_client_secret
+  }
+}
