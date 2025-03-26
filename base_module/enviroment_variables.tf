80f980ac5cadc15b873f391968a4923dd793145e
Simen Svenkerud <simen@ullrscrafts.com>
2025-03-26 16:45:06 +0100

    WIP

--- base_module/enviroment_variables.tf
+++ base_module/enviroment_variables.tf
@@ -1,32 +1,4 @@
-resource "dbtcloud_environment_variable" "dbt_db_env_var_snowflake" {
-  for_each   = toset(var.dbt_cloud_projects)
-  name       = "DBT_DB_ENV"
-  project_id = dbtcloud_project.dbt_project[each.key].id
-  environment_values = {
-    "Dev" : "dev"
-    "Prod" : "prod"
-  }
-  depends_on = [
-    dbtcloud_environment.snowflake_deployment_environment,
-    dbtcloud_environment.snowflake_dev_environment,
-    dbtcloud_environment.snowflake_prod_environment
-  ]
-}
-resource "dbtcloud_environment_variable" "dbt_schema_env_var_snowflake" {
-  for_each   = toset(var.dbt_cloud_projects)
-  name       = "DBT_SCHEMA_ENV"
-  project_id = dbtcloud_project.dbt_project[each.key].id
-  environment_values = {
-    "Dev" : "dev"
-    "Prod" : "prod"
-  }
-  depends_on = [
-    dbtcloud_environment.snowflake_deployment_environment,
-    dbtcloud_environment.snowflake_dev_environment,
-    dbtcloud_environment.snowflake_prod_environment
-  ]
-}
-resource "dbtcloud_environment_variable" "dbt_db_env_var_redshift" {
+resource "dbtcloud_environment_variable" "dbt_db_env_var" {
   for_each   = toset(var.dbt_cloud_projects)
   name       = "DBT_DB_ENV"
   project_id = dbtcloud_project.dbt_project[each.key].id
@@ -38,12 +10,15 @@ resource "dbtcloud_environment_variable" "dbt_db_env_var_redshift" {
   depends_on = [
     dbtcloud_environment.redshift_deployment_environment,
     dbtcloud_environment.redshift_dev_environment,
-    dbtcloud_environment.redshift_prod_environment
+    dbtcloud_environment.redshift_prod_environment,
+    dbtcloud_environment.snowflake_deployment_environment,
+    dbtcloud_environment.snowflake_dev_environment,
+    dbtcloud_environment.snowflake_prod_environment
   ]
 
 }
 
-resource "dbtcloud_environment_variable" "dbt_schema_env_var_redshift" {
+resource "dbtcloud_environment_variable" "dbt_schema_env_var" {
   for_each   = toset(var.dbt_cloud_projects)
   name       = "DBT_SCHEMA_ENV"
   project_id = dbtcloud_project.dbt_project[each.key].id
@@ -55,6 +30,9 @@ resource "dbtcloud_environment_variable" "dbt_schema_env_var_redshift" {
   depends_on = [
     dbtcloud_environment.redshift_deployment_environment,
     dbtcloud_environment.redshift_dev_environment,
-    dbtcloud_environment.redshift_prod_environment
+    dbtcloud_environment.redshift_prod_environment,
+    dbtcloud_environment.snowflake_deployment_environment,
+    dbtcloud_environment.snowflake_dev_environment,
+    dbtcloud_environment.snowflake_prod_environment
   ]
 }
