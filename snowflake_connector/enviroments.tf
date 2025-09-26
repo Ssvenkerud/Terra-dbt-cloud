5d4921ddeb6390bfccf0a265e627c16d634e1aed
Simen Svenkerud <simen@ullrscrafts.com>
2025-09-26 15:26:56 +0200

    disabel

--- snowflake_connector/enviroments.tf
+++ snowflake_connector/enviroments.tf
@@ -1,21 +1,21 @@
 
-resource "dbtcloud_environment" "snowflake_prod_environment" {
-  for_each = { for conn in var.dbt_cloud_snowflake_prod_environment :
-    conn.project =>
-  conn }
-  dbt_version       = var.dbt_cloud_version
-  name              = "prod"
-  project_id        = dbtcloud_project.dbt_project[each.key].id
-  type              = "deployment"
-  deployment_type   = "production"
-  credential_id     = dbtcloud_snowflake_credential.snowflake_pk_creds[each.value.snowflake_connection].credential_id
-  connection_id     = dbtcloud_global_connection.snowflake_non_sso[each.value.snowflake_connection].id
-  use_custom_branch = true
-  custom_branch     = var.dbt_prod_branch
-  depends_on = [dbtcloud_global_connection.snowflake_non_sso,
-    dbtcloud_snowflake_credential.snowflake_pk_creds
-  ]
-}
+#resource "dbtcloud_environment" "snowflake_prod_environment" {
+#  for_each = { for conn in var.dbt_cloud_snowflake_prod_environment :
+#    conn.project =>
+#  conn }
+#  dbt_version       = var.dbt_cloud_version
+#  name              = "prod"
+#  project_id        = dbtcloud_project.dbt_project[each.key].id
+#  type              = "deployment"
+#  deployment_type   = "production"
+#  credential_id     = dbtcloud_snowflake_credential.snowflake_pk_creds[each.value.snowflake_connection].credential_id
+#  connection_id     = dbtcloud_global_connection.snowflake_non_sso[each.value.snowflake_connection].id
+#  use_custom_branch = true
+#  custom_branch     = var.dbt_prod_branch
+#  depends_on = [dbtcloud_global_connection.snowflake_non_sso,
+#    dbtcloud_snowflake_credential.snowflake_pk_creds
+#  ]
+#}
 
 resource "dbtcloud_environment" "snowflake_manual_prod_environment" {
   for_each = { for conn in var.dbt_cloud_snowflake_prod_environment :
